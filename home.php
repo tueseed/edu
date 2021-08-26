f00baba32e3853299ab9e1b79beeb6c90ad46ed3
Nutthapong <tue_seed@hotmail.com>
2021-08-26 19:32:25 +0700

    pvnjfovnoidf

--- home.php
+++ home.php
@@ -7,25 +7,10 @@
     </div>
   </div>
 </div>
-
-
 <div class="row ml-2 mr-2 mt-3">
-  <!-- textarea สำหรับใช้แสดง ข้อความที่แปลงจากเสียงพูด-->
-  <!-- <textarea id="transcript" name="transcript" cols="50" rows="3"></textarea> -->
-  
-  <!--div นี้ เราจะใช้แสดง คำแนะนำ การใช้งาน หรือสถานะ-->
-  
-
-  <!--   ปุ่มนี้ เราใช้สำหรับควบคุมการทำงาน การจดจำเสียงพูดและแปลงเป็นข้อความ-->
-  <!-- <button class="btn btn-success" id="start_button">กดเพื่อเริ่ม</button>  -->
   <button class="btn btn-success ml-2" id="start_button" onclick="rnd_word()">คำต่อไป</button>    
-  <!-- <div id="instructions"></div> -->
-  </div>
 </div>
 
-<div class="row ml-2 mr-2 mt-3  ">
-  <span type="text" id="transcript" style="font-size:25px;"></span>
-</div>
 
 
 

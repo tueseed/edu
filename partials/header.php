fb3aa70fc2c078b6db17d948a6d6e5c369a62545
Nutthapong <tue_seed@hotmail.com>
2021-08-26 19:12:49 +0700

    vncklsnvk

--- partials/header.php
+++ partials/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>ระบบยืมหนังสือผ่านเว็บแอพพลิเคชั่น</title>
+    <title>บัญชีคำพื้นรฐาน ป.1</title>
     <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
     <link rel="stylesheet" href="./assets/css/theme_1545570683953.css">
     <!-- <link rel="stylesheet" href="./assets/css/star-rating.css"> -->
@@ -12,15 +12,16 @@
     <link rel="stylesheet" href="https://unpkg.com/bootstrap-table@1.14.2/dist/bootstrap-table.min.css">
     <link rel="stylesheet" href="./assets/css/bootstrap-table-sticky-header.css">
     <link rel="stylesheet" href="./assets/css/effect.css">
-    <link rel="stylesheet" href="./assets/css/sidemenu.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.8.0/css/bootstrap-datepicker.min.css" />
     <!-- <link rel="icon" href="./pic/logo-auc.png" type="image/gif" sizes="64x64"> -->
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Prompt&display=swap" rel="stylesheet">
-
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Sarabun:wght@100&display=swap" rel="stylesheet">
     <style>
       * {
-        font-family: 'Prompt', sans-serif;
+        font-family: 'Sarabun', sans-serif;
       }
     </style>
   </head>
@@ -31,7 +32,7 @@
       <nav class="shadow-sm navbar navbar-light shadow" style = "background-color: #fad2e1;">
         <div class="container-fluid">
           <a class="navbar-brand text-dark " href="?action=home" style="font-size: 36px;">
-            มาอ่านกันเถอะ
+          บัญชีคำพื้นรฐาน ป.1
           </a>
 
         </div>
